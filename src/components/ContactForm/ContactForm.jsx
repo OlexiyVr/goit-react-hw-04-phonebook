01aca89ab2a73e889bd16bad85a748ddb66dab6f
OlexiyVr <olexiyvr@gmail.com>
2023-06-15 22:42:14 +0300

    scscsc

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -23,7 +23,7 @@ export const ContactForm = ({ getSubmitData }) => {
               <Input
                 type="text"
                 name="name"
-                pattern="^[a-zA-Zа-яА-ЯёЁ]+(([\' -][a-zA-Zа-яА-ЯёЁ ])?[a-zA-Zа-яА-ЯёЁ]*)*$"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
               />
